a93db464aa80d6aafbb26b6a5f648ed5ac37d6dd
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-08-05 23:18:55 +0530

    Create Divide.java

---
+++ Divide.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+        while (n1 > n2) {
+            n1 = n1 - n2;
+        }
+
+        if (n1 == n2 || n1 == 0)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
