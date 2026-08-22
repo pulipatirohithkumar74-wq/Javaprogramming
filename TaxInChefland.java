f6acbe0866d89a44a30dec410947ed7322360103
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-08-22 23:13:00 +0530

    Add TaxInChefland.java for tax calculation

---
+++ TaxInChefland.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int X = sc.nextInt();
+
+            if (X > 100) {
+                System.out.println(X - 10);
+            } else {
+                System.out.println(X);
+            }
+        }
+    }
+}
