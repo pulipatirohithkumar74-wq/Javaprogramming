f37d246d01c6fe7c1a362f5c0a3baf14313b680e
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-08-28 22:30:27 +0530

    Implement method to find missing number in array

---
+++ Find missing number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = n * (n + 1) / 2;
+
+        for (int x : nums) {
+            sum -= x;
+        }
+
+        return sum;
+    }
+}
