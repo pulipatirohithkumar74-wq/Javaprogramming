ddc269c7305ce284609be25b4668c5375f16e66f
pulipatirohithkumar74-wq <pulipatirohithkumar74@gmail.com>
2026-09-25 21:53:16 +0530

    Implement GCD calculation for an array of numbers

---
+++ GCD of array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int ans = sc.nextInt();
+
+        for (int i = 1; i < n; i++)
+            ans = gcd(ans, sc.nextInt());
+
+        System.out.println(ans);
+    }
+}
